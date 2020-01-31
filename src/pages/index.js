0bc3960c73ddd4ee3e74bb8af30be2546d859792
kibalion2427 <roger1994a@gmail.com>
2020-01-30 19:57:38 -0500

    fixed contentful img

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,6 +40,7 @@ export const query = graphql`
           category
           image {
             fixed(width: 50, height: 50) {
+              src
               ...GatsbyContentfulFixed_tracedSVG
             }
           }
